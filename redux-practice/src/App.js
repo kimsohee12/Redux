c2b0a529e7e96678fcfe3ce1064013a8a88dd8fa
김소희 <130417959+kimsohee12@users.noreply.github.com>
2023-07-19 23:47:43 +0900

    redux 셋업

---
+++ redux-practice/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [count,setCount]=useState(0);
+
+  const increase=()=>{
+    setCount(count+1)
+  };
+
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increase}>증가</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ redux-practice/src/redux/reducer/reducer.js
@@ -0,0 +1,12 @@
+// 어떤 state 가 있는지 초기화 시켜주기
+let initalState ={
+    count:0
+}
+
+
+// (저장할 값 , 어떤 action에 따라 행동해야할지)
+function reducer(state=initalState,action){
+
+}
+
+export default reducer
