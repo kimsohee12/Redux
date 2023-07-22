08b1b3103c7255238556aa77535891f43d2d711c
김소희 <130417959+kimsohee12@users.noreply.github.com>
2023-07-22 20:06:53 +0900

    Redux 기초

--- redux-practice/src/App.js
+++ redux-practice/src/App.js
@@ -1,18 +1,35 @@
 import './App.css';
-import { useState } from 'react';
+import Box from "./component/Box";
+import {useDispatch, useSelector} from 'react-redux';
+
 
 function App() {
-  const [count,setCount]=useState(0);
+  const dispatch = useDispatch();
+  const count = useSelector(state=>state.count);
 
+
+  let id = useSelector(state =>state.id)
+  let pw = useSelector(state =>state.password)
   const increase=()=>{
-    setCount(count+1)
+    // dispatch 가 액션을 가져가서 reducer에 전달
+    dispatch({type :"INCREMENT" , payload : {num : 5} })//{타입 : , 옵션 (필요한 정보를 보내줄 수 있는 옵션): }
+  };
+  const login =()=>{
+    dispatch({type : "LOGIN" , payload : {id : "sohee" , password : "1234"} })
+  };
+  const discrease =()=>{
+    dispatch({type : "DISINCREMENT" , payload : {num:5} })
   };
 
 
   return (
     <div>
+      <h1>{id}{pw}</h1>
       <h1>{count}</h1>
       <button onClick={increase}>증가</button>
+      <button onClick={discrease}>감소</button>
+      <button onClick={login}>login! </button>
+      <Box/>
     </div>
   );
 }
--- redux-practice/src/redux/reducer/reducer.js
+++ redux-practice/src/redux/reducer/reducer.js
@@ -1,12 +1,22 @@
 // 어떤 state 가 있는지 초기화 시켜주기
 let initalState ={
-    count:0
+    count:0,
+    id : " ",
+    password : " "
 }
 
 
-// (저장할 값 , 어떤 action에 따라 행동해야할지)
-function reducer(state=initalState,action){
+// (저장할 값 , 어떤 action에 따라 행동해야할지) -> 행동 지침 역학
+function reducer(state=initalState , action){
+    if(action.type === "INCREMENT"){
+        return {...state,count:state.count + action.payload.num}; //...state = > state 값은 유지하되 count의 값만 바꿈
+    }else if(action.type === "LOGIN"){
+        return {...state , id : action.payload.id  , password : action.payload.password}
+    }else if (action.type ==="DISINCREMENT"){
+        return {...state , count : state.count - action.payload.num}
+    }
 
-}
+    return {...state};
+};
 
-export default reducer
+export default reducer;
